329d24fe96a7f6f47b135fd9a906d549857b9bf2
Kyle Yen <kyle870216@gmail.com>
2022-03-10 19:16:57 +0800

    Add output file and candidate 1

--- hw1/110062643_hw1.py
+++ hw1/110062643_hw1.py
@@ -1,39 +1,58 @@
 import sys
 from apyori import apriori # for test
+from collections import Counter
 
+
+def apriori_myself(transations, min_support):
+    association_results = []
+    init = []
+
+    for transation in transations:
+        for t in transation:
+            if t not in init:
+                init.append(t)
+
+    # sort init
+    init = [int(x) for x in init]
+    init = sorted(init)
+    init = [str(x) for x in init]
+
+    s = int(min_support * len(init))    #calculate iteration
+
+    c = Counter()
+    for i in init:
+        for transation in transations:
+            if i in transation:
+                c[i] += 1
+    print('C1:')
+    for i in c:
+        print(str(i)+ ':' +str(c[i]))
+
+    return association_results
+
+
+# read data
 data = []
+results = []
 with open(sys.argv[2], 'r', encoding='utf-8') as f:
     for line in f.readlines():
         line = line.rstrip().split(',')
         data.append(line)
-    
-    # print(data)
-    # print(data1)
-    # data2 = [['r', 'z', 'h', 'j', 'p'],
-    #    ['z', 'y', 'x', 'w', 'v', 'u', 't', 's'],
-    #    ['z'],
-    #    ['r', 'x', 'n', 'o', 's'],
-    #    ['y', 'r', 'x', 'z', 'q', 't', 'p'],
-    #    ['y', 'z', 'x', 'e', 'q', 's', 't', 'm']]
-    # print(type(data2[0][0]))
-    association_rules = apriori(data, min_support=float(sys.argv[1]))
-    association_results = list(association_rules)
 
-    print(len(association_results))
-    results = []
-    for item in association_results:
-        pair = item[0]
-        items = [x for x in pair]
-        items = ','.join(sorted(items))
-        support = float(item[1])
-        results.append(items + ':' + '%.04f'%support)
+mim_support = float(sys.argv[1])
 
-    print(results)
-    # for item in association_results:
-    #     pair = item[0] 
-    #     items = [x for x in pair]
-    #     print("Rule: " + items[0] + " -> " + items[1])
-    #     print("Support: " + str(item[1]))
-    #     print("Confidence: " + str(item[2][0][2]))
-    #     print("Lift: " + str(item[2][0][3]))
-    #     print("=====================================")
+results = apriori_myself(data, mim_support)
+# association_rules = apriori(data, min_support=float(sys.argv[1]))
+# association_results = list(association_rules)
+
+# for item in association_results:
+#     pair = item[0]
+#     items = [x for x in pair]
+#     items = ','.join(sorted(items))
+#     support = float(item[1])
+#     results.append(items + ':' + '%.04f'%support)
+
+# output file
+with open(sys.argv[3], 'w', encoding='utf-8') as f:
+    for result in results:
+        f.write(result+'\n')
