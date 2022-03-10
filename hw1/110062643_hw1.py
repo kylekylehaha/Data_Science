4bbce48b5314c7ad4bb49ab0b9bf9da9f2d8fe8e
Kyle Yen <kyle870216@gmail.com>
2022-03-10 19:50:17 +0800

    Add Apriori algo

--- hw1/110062643_hw1.py
+++ hw1/110062643_hw1.py
@@ -17,7 +17,7 @@ def apriori_myself(transations, min_support):
     init = sorted(init)
     init = [str(x) for x in init]
 
-    s = int(min_support * len(init))    #calculate iteration
+    s = int(min_support * len(init))    # mim support count
 
     c = Counter()
     for i in init:
@@ -26,7 +26,64 @@ def apriori_myself(transations, min_support):
                 c[i] += 1
     print('C1:')
     for i in c:
-        print(str(i)+ ':' +str(c[i]))
+        print(str(i)+ ':' + str(c[i]))
+    print()
+
+    l = Counter()
+    for i in c:
+        if c[i] >= s:
+            l[frozenset([i])] += c[i]
+
+    print('L1:')
+    for i in l:
+        print(str(i) + ':' + str(l[i]))
+    print()
+
+    pl = 1
+    pos = 1
+
+    for count in range(2, 1000):
+        nc = set()
+        temp = list(l)
+        for i in range(0, len(temp)):
+            for j in range(i+1, len(temp)):
+                t = temp[i].union(temp[j])
+                if len(t) == count:
+                    nc.add(temp[i].union(temp[j]))
+
+        nc = list(nc)
+        c = Counter()
+        for i in nc:
+            c[i] = 0
+            for transation in transations:
+                temp = set(transation)
+                if i.issubset(temp):
+                    c[i] += 1
+        print('C' + str(count) + ':')
+        for i in c:
+            print(str(i) + ':' +str(c[i]))
+        print()
+
+        l = Counter()
+        for i in c:
+            if(c[i] >= s):
+                l[i]+=c[i]
+        print("L"+str(count)+":")
+        for i in l:
+            print(str(list(i))+": "+str(l[i]))
+        print()
+
+        if len(l) == 0:
+            break
+        
+        pl = l
+        pos = count
+
+    print("Result: ")
+    print("L"+str(pos)+":")
+    for i in pl:
+        print(str(list(i))+": "+str(pl[i]))
+    print()
 
     return association_results
 
